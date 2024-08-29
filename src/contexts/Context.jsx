fd1a011109b6dd8f7188bf7957ba158926708cc8
iSayZ <estrine.alexis@gmail.com>
2024-08-29 17:35:27 +0200

    New project

--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -10,7 +10,27 @@ export function MainProvider({ children }) {
 
     const projects = [
       {
-          id: 1,
+        id: 1,
+        name: "Spot Lille Art",
+        desc: "Spot Lille Art est une plateforme participative permettant de référencer, partager et découvrir des œuvres de street art dans la Métropole Européenne de Lille. Le projet inclut la mise en place d'un CRUD et de routes d'API sécurisées, une authentification, une carte interactive, la prise et l'upload de photos, ainsi qu'un espace administrateur.",
+        techno: ["React", "Express", "MySQL"],
+        src: "https://spot-lille-art.utopland.net",
+        gitSrc: "https://github.com/WildCodeSchool-2024-02/LILLE-0224-P3-G2-streetart",
+        date: "Juillet 2024",
+        time: "6 semaines",
+        color: {carouselBubble: "cyan-500", carouselArrow: "white", other: "cyan"},
+        imgTop: 0,
+        img: [
+            "../assets/projects/spot-lille-art/artworks.png",
+            "../assets/projects/spot-lille-art/artwork.png",
+            "../assets/projects/spot-lille-art/map.png",
+            "../assets/projects/spot-lille-art/register.png",
+            "../assets/projects/spot-lille-art/profil.png"
+        ],
+        key: ["all", "team"]
+      },
+      {
+          id: 2,
           name: "MineGuide",
           desc: "Site réalisé en équipe lors de l'événement 'ProtoJam' de la Wild Code School, un concours où nous avions 24 heures pour développer un site autour du thème 'Nature & Déconnexion'. Nous avons choisi de prendre le sujet à l’envers. En hommage à la période du Covid où nous nous sommes évadés à travers les jeux vidéo, nous avons choisi Minecraft ! Ce jeu emblématique nous plonge dans des environnements naturels tout en nous offrant une évasion totale de la réalité. Ce site est donc une expérience fictive où vous pouvez découvrir 4 biomes de Minecraft, où chaque membre de notre équipe propose une location de gîte. Ce projet a non seulement été un plaisir à créer, mais il a également été récompensé par le prix de la meilleure technique lors de l'événement ! 🏆 (Le site est optimisé uniquement pour les ordinateurs en raison de contraintes de temps)",
           techno: ["React", "JavaScript", "CSS"],
@@ -32,7 +52,7 @@ export function MainProvider({ children }) {
           key: ["all", "team", "small"]
       },
       {
-          id: 2,
+          id: 3,
           name: "Feet & Fun",
           desc: `Feet & Fun est un site de e-commerce (basé sur la vente de chausettes 🧦) réalisé en équipe.
           Nous avons créé notre propre API qui contient toutes les informations de nos 150 produits.
@@ -62,13 +82,13 @@ export function MainProvider({ children }) {
           key: ["all", "team"]
       },
       {
-          id: 3,
+          id: 4,
           name: "Portfolio",
           desc: "Mon portfolio professionnel (site sur lequel vous naviguez).",
           techno: ["React", "JavaScript", "Tailwind CSS", "Responsive"],
           src: "https://estrine-alexis.vercel.app/",
           gitSrc: "https://github.com/iSayZ/Portfolio",
-          date: "Avril 2024",
+          date: "Mai 2024",
           color: {carouselBubble: "cyan-500", carouselArrow: "white", other: "cyan"},
           imgTop: 0,
           img: [
@@ -77,7 +97,7 @@ export function MainProvider({ children }) {
           key: ["all", "only"]
       },
       {
-          id: 4,
+          id: 5,
           name: "Trombinoscope",
           desc: "Trombinoscope réalisé en équipe, mettant en valeur la promotion de la Wild Code School 2024.",
           techno: ["JavaScript", "HTML", "CSS", "Responsive"],
